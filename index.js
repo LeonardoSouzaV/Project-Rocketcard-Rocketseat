5396282c975b28b636ba251bc155ad27d7dcddec
Leonardo S <vleo_souza@hotmail.com>
2023-05-01 14:57:53 -0300

    Refactoring off Function 'ChangeCardColor'

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-function GenerateRandomColors() {
-  const hex = 0xffffff
-  let number = Math.random() * hex
-  number = Math.floor(number)
-  number = number.toString(16)
-
-  const color = number.padStart(6, 0)
-  return `#${color}`
-}
-
 function ChangeCardColor() {
-  const backgroundColor = GenerateRandomColors()
+  const borderColor = "#" + Math.round(Math.random() * 0xffffff).toString(16)
 
-  document.querySelector("div.card-color").style.backgroundColor =
-    backgroundColor
+  document.querySelector("div.card-color").style.backgroundColor = borderColor
 }
 
 const url = "https://api.github.com/users/LeonardoSouzaV"
@@ -24,12 +13,12 @@ function GetDatasFromAPIGitHub(url) {
       const data = response.data
 
       document.querySelector("a#github-user").textContent = data.login
-      document.querySelector("span#location").textContent = data.location
-      document.querySelector("span#company").textContent = data.company
       document.querySelector("span#followers").textContent = data.followers
       document.querySelector("span#following").textContent = data.following
       document.querySelector("span#repositories").textContent =
         data.public_repos
+      document.querySelector("span#location").textContent = data.location
+      document.querySelector("span#company").textContent = data.company
       document.querySelector("img#github-user-photo").src = data.avatar_url
     })
     .catch((error) => prompt.error(error))
